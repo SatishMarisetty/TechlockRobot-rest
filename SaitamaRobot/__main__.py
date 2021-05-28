5a15fe58c747fac343faac54f9042b81868b665a
SatishMarisetty <79030362+SatishMarisetty@users.noreply.github.com>
2021-05-28 06:16:01 +0530

    Update __main__.py

--- SaitamaRobot/__main__.py
+++ SaitamaRobot/__main__.py
@@ -250,7 +250,7 @@ def start(update: Update, context: CallbackContext):
                         ],
                         
                         [
-                            InlineKeyboardButton(text="⚙ Help & Commands", callback_data="help_back"),
+                            InlineKeyboardButton(text="⚙ Help & Commands",  url="https://t.me/techlock_bot?start=help"),
                         ],
                     ],
                 ),
