28733a3405fea297f8e65b3e03bed2166c0067a8
SatishMarisetty <79030362+SatishMarisetty@users.noreply.github.com>
2021-05-29 18:34:27 +0530

    Update blacklist_stickers.py

--- SaitamaRobot/modules/blacklist_stickers.py
+++ SaitamaRobot/modules/blacklist_stickers.py
@@ -494,7 +494,7 @@ def __stats__():
     )
 
 
-__mod_name__ = "Stickers Blacklist"
+__mod_name__ = "STICKERS-BLACKLIST"
 
 BLACKLIST_STICKER_HANDLER = DisableAbleCommandHandler(
     "blsticker", blackliststicker, admin_ok=True,
