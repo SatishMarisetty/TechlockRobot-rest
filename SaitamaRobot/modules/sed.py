4fc5b5c98f2bbc8a848664ed1bde052e2abf0914
SatishMarisetty <79030362+SatishMarisetty@users.noreply.github.com>
2021-05-29 18:56:25 +0530

    Update sed.py

--- SaitamaRobot/modules/sed.py
+++ SaitamaRobot/modules/sed.py
@@ -135,7 +135,7 @@ If you want to use these characters, make sure you escape them!
     telegram.MAX_MESSAGE_LENGTH,
 )
 
-__mod_name__ = "Sed/Regex"
+__mod_name__ = "SED/REGEX"
 
 SED_HANDLER = DisableAbleMessageHandler(
     Filters.regex(r"s([{}]).*?\1.*".format("".join(DELIMITERS))), sed, friendly="sed",
