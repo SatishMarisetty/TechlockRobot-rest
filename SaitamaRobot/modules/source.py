d9bc31e4192014ce29d4f09987954785d12a0002
SatishMarisetty <79030362+SatishMarisetty@users.noreply.github.com>
2021-05-29 20:48:35 +0530

    Update source.py

--- SaitamaRobot/modules/source.py
+++ SaitamaRobot/modules/source.py
@@ -1,4 +1,4 @@
 from SaitamaRobot import dispatcher
 
-dispatcher.add_handler(commands=["source"])
+dispatcher.CommandHandler(commands=["source"])
 def sendmessage(message): bot.reply_to(message, "🖕")
