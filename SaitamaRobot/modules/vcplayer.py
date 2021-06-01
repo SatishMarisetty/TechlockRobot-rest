19c3581d93c6bfc9ef46c2a84ca07a155c0ff3c1
SatishMarisetty <79030362+SatishMarisetty@users.noreply.github.com>
2021-06-01 21:36:16 +0530

    Update and rename vcplayer.txt to vcplayer.py

---
+++ SaitamaRobot/modules/vcplayer.py
@@ -0,0 +1,37 @@
+
+__mod_name__ = "VC PLAYER"
+__help__ = """
+          》VC PLAYER《
+            
+I can also play music in your group's voice chat
+
+MUSIC ASSISTANT:- @Techlockmusicassistant
+ 
+ Setting up 
+*use /userbotjoin command
+1) Make bot admin
+2) Start a voice chat
+3) Try `/play [song name]` for the first time by an admin
+) If userbot joined enjoy music.
+ Commands:
+ 
+• Song Playing 🎶
+  `/play`: Play song using youtube music
+  `/play` <yt url> : Play the given yt url
+  `/dplay`: Play song via deezer
+  `/splay`: Play song via jio saavn
+  
+• Playback ⏯
+  `/player`: Open Settings menu of player
+  `/skip`: Skips the current track
+  `/pause`: Pause track
+  `/resume`: Resumes the paused track
+  `/end`: Stops media playback
+  `/current`: Shows the current Playing track
+  `/playlist`: Shows playlist
+• More tools 📲
+  `/admincache`: Updates admin info of your group. Try if bot isn't recognize admin
+  /userbotjoin: Invite @Techlockmusicassistant to your chat
+Player cmd and all other cmds except /play, /current and /playlist are only for admins with manage group
+ PLEASE NOTE THIS SERVICE IS UNSTABLE AND CAN BE STOPPED ANYTIME 
+"""
