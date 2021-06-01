d2c753ded5f831d06b5691e08d2a728ed75c3d40
SatishMarisetty <79030362+SatishMarisetty@users.noreply.github.com>
2021-06-01 07:26:19 +0530

    Update misc.py

--- SaitamaRobot/modules/helper_funcs/misc.py
+++ SaitamaRobot/modules/helper_funcs/misc.py
@@ -71,7 +71,7 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
              EqInlineKeyboardButton("➡️", callback_data="{}_next({})".format(prefix, modulo_page)))]
 
     else:
-        pairs += [[EqInlineKeyboardButton("Back", callback_data="saitama_back")]]
+        pairs += [[EqInlineKeyboardButton("🔙", callback_data="saitama_back")]]
 
     return pairs
 
