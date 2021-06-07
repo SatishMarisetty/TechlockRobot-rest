31959096b4cfa6bee03ab7d0fcc39d1058360d52
Deadpool <79030362+SatishMarisetty@users.noreply.github.com>
2021-06-07 19:59:13 +0530

    Update __main__.py

--- SaitamaRobot/__main__.py
+++ SaitamaRobot/__main__.py
@@ -657,7 +657,7 @@ def main():
             updater.bot.set_webhook(url=URL + TOKEN)
 
     else:
-        LOGGER.info("Eren Jaeger is deployed successfully!")
+        LOGGER.info("TECHLOCK is deployed successfully!")
         updater.start_polling(timeout=15, read_latency=4, clean=True)
 
     if len(argv) not in (1, 3, 4):
