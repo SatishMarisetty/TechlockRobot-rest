24708532fd942e1b78ea507da58a49e124f09f7e
Deadpool <79030362+SatishMarisetty@users.noreply.github.com>
2021-06-10 07:33:33 +0530

    Update game.py

--- SaitamaRobot/modules/game.py
+++ SaitamaRobot/modules/game.py
@@ -62,13 +62,3 @@ async def _(event):
         except BaseException:
             pass
 
-
-
-__help__ = """
- *Play Game With Emojis:*
-  - /dice or /dice 1 to 6 any value
-  - /ball or /ball 1 to 5 any value
-  - /dart or /dart 1 to 6 any value
- Usage: hahaha just a magic.
- warning: you would be in trouble if you input any other value than mentioned.
-"""
