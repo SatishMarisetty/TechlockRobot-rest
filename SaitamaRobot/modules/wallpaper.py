f94c6348fff469c568f57e5e30f5b81dbedb520e
Deadpool <79030362+SatishMarisetty@users.noreply.github.com>
2021-06-12 13:10:00 +0530

    Update wallpaper.py

--- SaitamaRobot/modules/wallpaper.py
+++ SaitamaRobot/modules/wallpaper.py
@@ -22,9 +22,9 @@ def wall(update: Update, context: CallbackContext):
         return
     else:
         caption = query
-        term = query.replace(" ", "%20")
+        term = query.replace(" ", "+")
         json_rep = r.get(
-            f"https://wall.alphacoders.com/api2.0/get.php?auth={WALL_API}&method=search&term={term}",
+            f"https://pixabay.com/api/?key=22046498-754caf17e3ab2ce976b598882&q={term}&image_type=photo&pretty=true",
         ).json()
         if not json_rep.get("success"):
             msg.reply_text(f"An error occurred! Report this @{SUPPORT_CHAT}")
@@ -36,8 +36,7 @@ def wall(update: Update, context: CallbackContext):
             else:
                 index = randint(0, len(wallpapers) - 1)  # Choose random index
                 wallpaper = wallpapers[index]
-                wallpaper = wallpaper.get("url_image")
-                wallpaper = wallpaper.replace("\\", "")
+                wallpaper = wallpaper.get("pageURL")
                 bot.send_photo(
                     chat_id,
                     photo=wallpaper,
