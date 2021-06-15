8a432df50908c9a39bb14dd2f3a32ff0478bbf33
Deadpool <79030362+SatishMarisetty@users.noreply.github.com>
2021-06-15 17:16:14 +0530

    Update telethonbasics.py

--- SaitamaRobot/utils/telethonbasics.py
+++ SaitamaRobot/utils/telethonbasics.py
@@ -113,15 +113,16 @@ async def get_all_admin_chats(event):
     return lul_stark
 
 
+
 async def is_admin(event, user):
     try:
         sed = await event.client.get_permissions(event.chat_id, user)
         if sed.is_admin:
             is_mod = True
         else:
-            is_mod = True
+            is_mod = False
     except:
-        is_mod = True
+        is_mod = False
     return is_mod
 
 
