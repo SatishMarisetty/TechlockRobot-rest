6f8c6ae7058f88c7af359323286ea7f0b9f067f8
Deadpool <79030362+SatishMarisetty@users.noreply.github.com>
2021-06-16 06:46:06 +0530

    Update shield.py

--- SaitamaRobot/modules/shield.py
+++ SaitamaRobot/modules/shield.py
@@ -28,7 +28,7 @@ from telethon.tl.types import ChatBannedRights
 from SaitamaRobot import BOT_ID
 from SaitamaRobot.conf import get_int_key, get_str_key
 
-from SaitamaRobot.db.mongo_helpers.nsfw_guard import add_chat, get_all_nsfw_chats, is_chat_in_db, rm_chat
+#from SaitamaRobot.db.mongo_helpers.nsfw_guard import add_chat, get_all_nsfw_chats, is_chat_in_db, rm_chat
 from SaitamaRobot.utils.telethonbasics import is_admin
 from SaitamaRobot.events import register
 from SaitamaRobot import MONGO_URI 
