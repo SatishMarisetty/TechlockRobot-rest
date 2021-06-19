ff3910eae27b4886f155ee10e0a7f41e9afaf914
Deadpool <79030362+SatishMarisetty@users.noreply.github.com>
2021-06-19 20:26:24 +0530

    Update __init__.py

--- SaitamaRobot/__init__.py
+++ SaitamaRobot/__init__.py
@@ -172,6 +172,7 @@ else:
     SPAMWATCH_SUPPORT_CHAT = Config.SPAMWATCH_SUPPORT_CHAT
     SPAMWATCH_API = Config.SPAMWATCH_API
     INFOPIC = Config.INFOPIC
+    TEMP_DOWNLOAD_DIRECTORY = Config.TEMP_DOWNLOAD_DIRECTORY
 
     try:
         BL_CHATS = set(int(x) for x in Config.BL_CHATS or [])
