5b9919b43f521dd64b34ef8aeea4762de3766abe
Deadpool <79030362+SatishMarisetty@users.noreply.github.com>
2021-06-20 14:03:51 +0530

    Update tts-stt.py

--- SaitamaRobot/modules/tts-stt.py
+++ SaitamaRobot/modules/tts-stt.py
@@ -177,14 +177,14 @@ async def _(event):
 __mod_name__ = "TTS/STT"
 
 __help__ = """
-Admin only or bot's PM
  - /tts <lang code>: Reply to any message to get text to speech output
  - /stt: Type in reply to a voice message(english only) to extract text from it.
+
 *Language Codes*
-af,am,ar,az,be,bg,bn,bs,ca,ceb,co,cs,cy,da,de,el,en,eo,es,
+`af,am,ar,az,be,bg,bn,bs,ca,ceb,co,cs,cy,da,de,el,en,eo,es,
 et,eu,fa,fi,fr,fy,ga,gd,gl,gu,ha,haw,hi,hmn,hr,ht,hu,hy,
 id,ig,is,it,iw,ja,jw,ka,kk,km,kn,ko,ku,ky,la,lb,lo,lt,lv,mg,mi,mk,
 ml,mn,mr,ms,mt,my,ne,nl,no,ny,pa,pl,ps,pt,ro,ru,sd,si,sk,sl,
 sm,sn,so,sq,sr,st,su,sv,sw,ta,te,tg,th,tl,tr,uk,ur,uz,
-vi,xh,yi,yo,zh,zh_CN,zh_TW,zu
+vi,xh,yi,yo,zh,zh_CN,zh_TW,zu`
 """
