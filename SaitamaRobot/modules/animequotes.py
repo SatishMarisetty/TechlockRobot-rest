fbe1626cbe63a7115706ba7288cd22e29d5a9ea8
Deadpool <79030362+SatishMarisetty@users.noreply.github.com>
2021-06-21 15:57:30 +0530

    Update animequotes.py

--- SaitamaRobot/modules/animequotes.py
+++ SaitamaRobot/modules/animequotes.py
@@ -34,19 +34,10 @@ def animequotes(update: Update, context: CallbackContext):
 
         random.choice(animequotes_strings.QUOTES_IMG))
 
-__help__ = """
-
- • `/animequotes`*:* gives random anime quotes
-
- 
-
-"""
 
 ANIMEQUOTES_HANDLER = DisableAbleCommandHandler("animequotes", animequotes)
 
 dispatcher.add_handler(ANIMEQUOTES_HANDLER)
-
-__mod_name__ = "ANIMEQUOTES"
 
 __command_list__ = [
 
