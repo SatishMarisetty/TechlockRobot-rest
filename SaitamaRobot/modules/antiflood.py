62a10e31c05dc71c88ae6d3fdada33c5716e595e
Deadpool <79030362+SatishMarisetty@users.noreply.github.com>
2021-06-21 15:58:08 +0530

    Update antiflood.py

--- SaitamaRobot/modules/antiflood.py
+++ SaitamaRobot/modules/antiflood.py
@@ -418,7 +418,7 @@ will result in restricting that user.
  `1w` = 1 week
  """
 
-__mod_name__ = "ANTI-FLOOD"
+__mod_name__ = "ANTIFLOOD"
 
 FLOOD_BAN_HANDLER = MessageHandler(
     Filters.all & ~Filters.status_update & Filters.group, check_flood,
