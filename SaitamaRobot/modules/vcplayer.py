8f4b279e928e2ed9e3eb754f15aec3c0c5ce430f
Deadpool <79030362+SatishMarisetty@users.noreply.github.com>
2021-06-22 09:50:20 +0530

    Update vcplayer.py

--- SaitamaRobot/modules/vcplayer.py
+++ SaitamaRobot/modules/vcplayer.py
@@ -1,26 +1,5 @@
-# Copyright (C) 2021 TeamDaisyX
-
-
-# This file is part of Daisy (Telegram Bot)
-
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
-
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 __mod_name__ = "VC PLAYER"
-__help__ = """
-          》VC PLAYER《
-            
-*Techlock can also play music in your group's voice chat*
+__help__ = """*Techlock can also play music in your group's voice chat*
 - `/vchelp` : Gives you music player commands
 
 *TECHLOCK BROUGHT A NEW OPTION TO ON/OFF MUSIC PLAYER IN THIS RELEASE *
@@ -47,19 +26,10 @@ Assistant username:- @TechlockVCmusicassistant
   `/play` <yt url> : Play the given yt url
   `/dplay`: Play song via deezer
   `/splay`: Play song via jio saavn
-  
-• Playback ⏯
-  `/player`: Open Settings menu of player
-  `/skip`: Skips the current track
-  `/pause`: Pause track
-  `/resume`: Resumes the paused track
-  `/end`: Stops media playback
-  `/current`: Shows the current Playing track
-  `/playlist`: Shows playlist
 
 • More tools 📲
   `/admincache`: Updates admin info of your group. Try if bot isn't recognize admin
-  `/userbotjoin`: Invite @TechlockVCmusicassistant to your chat
+  `/userbotjoin`: Invites @TechlockVCmusicassistant to your chat
 
 Player cmd and all other cmds except /play, /current and /playlist are only for admins.
 """
