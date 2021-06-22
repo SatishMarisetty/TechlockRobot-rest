f2fa740e0e6345965aceea35d6757fc74675f5d9
Deadpool <79030362+SatishMarisetty@users.noreply.github.com>
2021-06-22 10:39:48 +0530

    Update vcplayer.py

--- SaitamaRobot/modules/vcplayer.py
+++ SaitamaRobot/modules/vcplayer.py
@@ -1,35 +1,42 @@
+# Copyright (C) 2021 TeamDaisyX
+
+
+# This file is part of Daisy (Telegram Bot)
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 __mod_name__ = "VC PLAYER"
 __help__ = """
           》VC PLAYER《
             
-*Techlock can also play music in your group's voice chat*
-- `/vchelp` : Gives you music player commands
-
-*TECHLOCK BROUGHT A NEW OPTION TO ON/OFF MUSIC PLAYER IN THIS RELEASE*
-- `/musicplayer' on/off 
+Techlock can also play music in your group's voice chat
  
 Assistant username:- @TechlockVCmusicassistant
  
-• Setting up ⚙
+ Setting up 
 1) Make bot admin
 2) Start a voice chat
-3) Use `/userbotjoin` for the first time by an admin.
-4) Or manually add @TechlockVCmusicassistant
-5) After userbot joined group use `/play [song name]`
-6) Userbot joins Voice Chat enjoy music.
-  *note* : This all process is required for first time only
-
+3) Try `/play [song name]` for the first time by an admin
+) If userbot joined enjoy music, If not add @TechlockVCmusicassistant to your group manually and retry
  Commands:
-
-• Commands Menu 📝
-  `/vchelp`: Shows all available music player commands.
  
 • Song Playing 🎶
   `/play`: Play song using youtube music
   `/play` <yt url> : Play the given yt url
   `/dplay`: Play song via deezer
   `/splay`: Play song via jio saavn
-
+  
 • Playback ⏯
   `/player`: Open Settings menu of player
   `/skip`: Skips the current track
