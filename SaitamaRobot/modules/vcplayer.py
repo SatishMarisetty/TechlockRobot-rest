39d68fb8795808484cef21dda0be2a75a6b371d7
Deadpool <79030362+SatishMarisetty@users.noreply.github.com>
2021-06-22 10:32:05 +0530

    Update vcplayer.py

--- SaitamaRobot/modules/vcplayer.py
+++ SaitamaRobot/modules/vcplayer.py
@@ -29,8 +29,18 @@ Assistant username:- @TechlockVCmusicassistant
   `/play` <yt url> : Play the given yt url
   `/dplay`: Play song via deezer
   `/splay`: Play song via jio saavn
+
+• Playback ⏯
+  `/player`: Open Settings menu of player
+  `/skip`: Skips the current track
+  `/pause`: Pause track
+  `/resume`: Resumes the paused track
+  `/end`: Stops media playback
+  `/current`: Shows the current Playing track
+  `/playlist`: Shows playlist
 • More tools 📲
   `/admincache`: Updates admin info of your group. Try if bot isn't recognize admin
   `/userbotjoin`: Invite @TechlockVCmusicassistant to your chat
 Player cmd and all other cmds except /play, /current and /playlist are only for admins with manage group
+ PLEASE NOTE THIS SERVICE IS UNSTABLE AND CAN BE STOPPED ANYTIME 
 """
