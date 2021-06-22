a73a4f9e4e0b4291f983b7d18dae924f2d4a208e
Deadpool <79030362+SatishMarisetty@users.noreply.github.com>
2021-06-22 09:43:10 +0530

    Update vcplayer.py

--- SaitamaRobot/modules/vcplayer.py
+++ SaitamaRobot/modules/vcplayer.py
@@ -20,8 +20,7 @@ __mod_name__ = "VC PLAYER"
 __help__ = """
           》VC PLAYER《
             
-Techlock can also play music in your group's voice chat
-
+*Techlock can also play music in your group's voice chat*
 - `/vchelp` : Gives you music player commands
 
 *TECHLOCK BROUGHT A NEW OPTION TO ON/OFF MUSIC PLAYER IN THIS RELEASE *
@@ -29,12 +28,19 @@ Techlock can also play music in your group's voice chat
  
 Assistant username:- @TechlockVCmusicassistant
  
- Setting up 
+• Setting up ⚙
 1) Make bot admin
 2) Start a voice chat
-3) Try `/play [song name]` for the first time by an admin
-) If userbot joined enjoy music, If not add @TechlockVCmusicassistant to your group manually and retry
+3) Use `/userbotjoin` for the first time by an admin.
+4) Or manually add @TechlockVCmusicassistant
+5) After userbot joined group use `/play [song name]`
+6) Userbot joins Voice Chat enjoy music.
+  *note* : This process is required for first time only
+
  Commands:
+
+• Commands Menu 📝
+  `/vchelp`: Shows all available music player commands.
  
 • Song Playing 🎶
   `/play`: Play song using youtube music
@@ -50,9 +56,10 @@ Assistant username:- @TechlockVCmusicassistant
   `/end`: Stops media playback
   `/current`: Shows the current Playing track
   `/playlist`: Shows playlist
+
 • More tools 📲
   `/admincache`: Updates admin info of your group. Try if bot isn't recognize admin
   `/userbotjoin`: Invite @TechlockVCmusicassistant to your chat
-Player cmd and all other cmds except /play, /current and /playlist are only for admins with manage group
- PLEASE NOTE THIS SERVICE IS UNSTABLE AND CAN BE STOPPED ANYTIME 
+
+Player cmd and all other cmds except /play, /current and /playlist are only for admins.
 """
