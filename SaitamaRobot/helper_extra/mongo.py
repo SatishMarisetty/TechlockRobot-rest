4f201119c5938442338f97080e412514ac014ff1
Deadpool <79030362+SatishMarisetty@users.noreply.github.com>
2021-06-25 07:20:21 +0530

    Update mongo.py

--- SaitamaRobot/helper_extra/mongo.py
+++ SaitamaRobot/helper_extra/mongo.py
@@ -20,11 +20,9 @@ from motor import motor_asyncio
 from odmantic import AIOEngine
 from pymongo import MongoClient
 from pymongo.errors import ServerSelectionTimeoutError
-
-from SaitamaRobot import log
 from SaitamaRobot.conf import get_int_key, get_str_key
 
-MONGO_URI = get_str_key("MONGO_URI")
+MONGO_URI = "mongodb+srv://Huntingbots:bots288@cluster0.phpct.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 MONGO_PORT = get_int_key("MONGO_PORT")
 MONGO_DB = "DaisyX"
 
@@ -38,4 +36,4 @@ engine = AIOEngine(motor, MONGO_DB)
 try:
     asyncio.get_event_loop().run_until_complete(motor.server_info())
 except ServerSelectionTimeoutError:
-    sys.exit(log.critical("Can't connect to mongodb! Exiting..."))
+    sys.exit(1)
