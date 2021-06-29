010efde2e0339b8faa875a159134e23d80ca7c0f
Deadpool <79030362+SatishMarisetty@users.noreply.github.com>
2021-06-29 21:03:21 +0530

    Update __main__.py

--- SaitamaRobot/__main__.py
+++ SaitamaRobot/__main__.py
@@ -313,7 +313,7 @@ def help_button(update, context):
         if mod_match:
             module = mod_match.group(1)
             text = (
-                "⚊❮❮❮❮ ｢  Menu  of  *{}*  module 」❯❯❯❯⚊\n".format(
+                "❮❮❮ Menu  of  *{}*  module ❯❯❯\n".format(
                     HELPABLE[module].__mod_name__
                 )
                 + HELPABLE[module].__help__
