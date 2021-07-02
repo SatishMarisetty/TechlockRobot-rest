255433db3243b7a691d7f0d39b2b1fea752c477b
Deadpool <79030362+SatishMarisetty@users.noreply.github.com>
2021-07-02 08:13:35 +0530

    Update Gtrans.py

--- SaitamaRobot/modules/Gtrans.py
+++ SaitamaRobot/modules/Gtrans.py
@@ -1,1 +1,32 @@
+from googletrans import Translator
+
+
+
+
+@ultroid_cmd(
+    pattern="tr",
+)
+async def _(event):
+    if len(event.text) > 3:
+        if not event.text[3] == " ":
+            return
+    input = event.text[4:6]
+    txt = event.text[7:]
+    xx = await eor(event, "`Translating...`")
+    if event.reply_to_msg_id:
+        previous_message = await event.get_reply_message()
+        text = previous_message.message
+        lan = input or "en"
+    elif input:
+        text = txt
+        lan = input or "en"
+    else:
+        return await eod(xx, f"`{hndlr}tr LanguageCode` as reply to a message", time=5)
+    translator = Translator()
+    try:
+        tt = translator.translate(text, dest=lan)
+        output_str = f"**TRANSLATED** from {tt.src} to {lan}\n{tt.text}"
+        await eor(xx, output_str)
+    except Exception as exc:
+        await eod(xx, str(exc), time=10)
 
