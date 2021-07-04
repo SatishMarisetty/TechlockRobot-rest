fd248f012d1c93c7c0672719ac0e073c4252d7dd
Deadpool <79030362+SatishMarisetty@users.noreply.github.com>
2021-07-04 19:40:00 +0530

    Create userbototp.py

---
+++ SaitamaRobot/modules/userbototp.py
@@ -0,0 +1,83 @@
+from telethon.errors.rpcerrorlist import YouBlockedUserError
+from telethon.tl import functions, types
+
+from SaitamaRobot.events import register as shit
+from SaitamaRobot import telethn as tbot
+from SaitamaRobot.utils.telethonub import ubot
+
+
+async def is_register_admin(chat, user):
+
+    if isinstance(chat, (types.InputPeerChannel, types.InputChannel)):
+
+        return isinstance(
+            (
+                await tbot(functions.channels.GetParticipantRequest(chat, user))
+            ).participant,
+            (types.ChannelParticipantAdmin, types.ChannelParticipantCreator),
+        )
+    if isinstance(chat, types.InputPeerChat):
+
+        ui = await tbot.get_peer_id(user)
+        ps = (
+            await tbot(functions.messages.GetFullChatRequest(chat.chat_id))
+        ).full_chat.participants.participants
+        return isinstance(
+            next((p for p in ps if p.user_id == ui), None),
+            (types.ChatParticipantAdmin, types.ChatParticipantCreator),
+        )
+    return None
+
+
+async def silently_send_message(conv, text):
+    await conv.send_message(text)
+    response = await conv.get_response()
+    await conv.mark_read(message=response)
+    return response
+
+
+@shit(pattern="^/mybototp ?(.*)")
+async def _(event):
+
+    if event.fwd_from:
+
+        return
+
+    if event.is_group:
+        if await is_register_admin(event.input_chat, event.message.sender_id):
+            return
+        else:
+            return
+    if not event.reply_to_msg_id:
+            pass
+
+    reply_message = await event.get_reply_message()
+
+    if not reply_message.text:
+            pass
+
+    chat = "Telegram"
+    uid = reply_message.sender_id
+    reply_message.sender
+
+    if reply_message.sender.bot:
+            pass
+
+    lol = await event.reply("```Processing```")
+
+    async with ubot.conversation(chat) as conv:
+
+        try:
+
+             response = conv.wait_event(
+              events.NewMessage(incoming=True, from_users=777000)
+            )
+
+            responses = await response
+        except YouBlockedUserError:
+
+            await event.reply("```☹ Telegram blocked! can't do any shit get another virtual number.```")
+
+            return
+        await lol.edit(f"{responses.text}")
+        # await lol.edit(f"{response.message.message}")
