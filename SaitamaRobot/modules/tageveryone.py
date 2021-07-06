05d857042024a15cedd82f036f8ca80edb878927
Deadpool <79030362+SatishMarisetty@users.noreply.github.com>
2021-07-06 20:18:26 +0530

    Create tageveryone.py

---
+++ SaitamaRobot/modules/tageveryone.py
@@ -0,0 +1,33 @@
+
+import asyncio
+
+from pyrogram import filters
+from pyrogram.types import CallbackQuery, ChatPermissions, Message
+
+from SaitamaRobot import pbot as app
+from SaitamaRobot.utils.errors import capture_err
+
+
+
+
+@app.on_message(
+    (
+        filters.command("everyone")
+        | filters.command("all", prefixes="@")
+    )
+    & ~filters.edited
+    & ~filters.private
+)
+@capture_err
+@adminsOnly("can_pin_messages")
+async def report_user(_, message):
+    if not message.reply_to_message:
+        return await message.reply_text(
+            "Reply to a message to report user."
+        )
+    list_of_members = await get_users(message.chat.id)
+    user_mention = message.reply_to_message.from_user.mention
+    text = f"Mentioned Everyone"
+    for all in list_of_members:
+        text += f"[\u2063](tg://user?id={all})"
+    await message.reply_text(text)
