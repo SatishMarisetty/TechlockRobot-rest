055119c541a8f0ee0b0a1571ae01c6d40ab9137a
Deadpool <79030362+SatishMarisetty@users.noreply.github.com>
2021-07-06 21:14:59 +0530

    Update errors.py

--- SaitamaRobot/utils/errors.py
+++ SaitamaRobot/utils/errors.py
@@ -47,7 +47,7 @@ def capture_err(func):
             )
             for x in error_feedback:
                 await pbot.send_message(
-                    EVENT_LOGS,
+                    errorlogger,
                     x
                 )
             raise err
