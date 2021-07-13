38c31c5b01adbe7cffbc61d83aeee50fae2a8f27
Deadpool <79030362+SatishMarisetty@users.noreply.github.com>
2021-07-13 07:37:26 +0530

    Update rmbg.py

--- SaitamaRobot/modules/rmbg.py
+++ SaitamaRobot/modules/rmbg.py
@@ -94,7 +94,7 @@ async def _(event):
         await event.reply("Background Removed in {} seconds".format(ms))
     else:
         await event.reply(
-            "remove.bg API returned Errors. Please report to @DaisySupport_Official\n`{}".format(
+            "remove.bg API returned Errors. Please report to @techlockgram_bot \n`{}".format(
                 output_file_name.content.decode("UTF-8")
             )
         )
