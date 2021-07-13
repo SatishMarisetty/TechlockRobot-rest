6050dbe7e70dc0946fdf0543f9b6edc76c97ef56
Deadpool <79030362+SatishMarisetty@users.noreply.github.com>
2021-07-13 21:18:37 +0530

    Update userinfo.py

--- SaitamaRobot/modules/userinfo.py
+++ SaitamaRobot/modules/userinfo.py
@@ -344,13 +344,17 @@ def info(update: Update, context: CallbackContext):
 
             os.remove(f"{user.id}.png")
 # Incase user don't have profile pic, send normal text
+       except IndexError:
+            message.reply_text(
+                text, parse_mode=ParseMode.HTML, disable_web_page_preview=True,
+            )
+
     else:
         message.reply_text(
             text, parse_mode=ParseMode.HTML, disable_web_page_preview=True,
         )
 
-    rep.delete()
-
+   rep.delete()
 
 @run_async
 def about_me(update: Update, context: CallbackContext):
