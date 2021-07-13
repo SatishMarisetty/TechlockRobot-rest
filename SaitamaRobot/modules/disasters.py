e33591ad85e806e9590db19b6149919a4b5c238f
Deadpool <79030362+SatishMarisetty@users.noreply.github.com>
2021-07-13 10:19:57 +0530

    Update disasters.py

--- SaitamaRobot/modules/disasters.py
+++ SaitamaRobot/modules/disasters.py
@@ -672,7 +672,7 @@ dispatcher.add_handler(SUPPORTLIST_HANDLER)
 dispatcher.add_handler(SUDOLIST_HANDLER)
 dispatcher.add_handler(DEVLIST_HANDLER)
 
-__mod_name__ = "DISASTER"
+__mod_name__ = "DISASTERS"
 __handlers__ = [
     SUDO_HANDLER,
     SUPPORT_HANDLER,
