3a74ace4f9527f5309583ba7aec91ff452ec200d
Deadpool <79030362+SatishMarisetty@users.noreply.github.com>
2021-07-14 12:02:01 +0530

    Update functions.py

--- SaitamaRobot/utils/functions.py
+++ SaitamaRobot/utils/functions.py
@@ -24,18 +24,24 @@ SOFTWARE.
 import codecs
 import pickle
 from asyncio import gather, get_running_loop
+from io import BytesIO
 from math import atan2, cos, radians, sin, sqrt
 from random import randint
+from re import findall
+from time import time
+from datetime import timedelta, datetime
 
 import aiofiles
 import aiohttp
 import speedtest
-from carbonnow import Carbon
 from PIL import Image, ImageDraw, ImageFilter, ImageFont
+from pyrogram.types import Message
 from wget import download
 
+from SaitamaRobot import aiohttpsession as aiosession
+from SaitamaRobot import arq
 from SaitamaRobot.utils import aiodownloader
-from SaitamaRobot.utils.fetch import fetch
+from SaitamaRobot.utils.http import get
 
 """
 Just import 'downloader' anywhere and do downloader.download() to
@@ -91,7 +97,9 @@ def generate_captcha():
     for t in range(4):
         letter = gen_letter()
         correct_answer += letter
-        draw.text((60 * t + 50, 15), letter, font=font, fill=rndColor2())
+        draw.text(
+            (60 * t + 50, 15), letter, font=font, fill=rndColor2()
+        )
     image = image.filter(ImageFilter.BLUR)
     image.save(file, "jpeg")
     return [file, correct_answer, wrong_answers]
@@ -115,29 +123,34 @@ def test_speedtest():
 
 
 async def file_size_from_url(url: str) -> int:
-    async with aiohttp.ClientSession() as session:
-        async with session.head(url) as resp:
-            size = int(resp.headers["content-length"])
+    async with aiosession.head(url) as resp:
+        size = int(resp.headers["content-length"])
     return size
 
 
 async def get_http_status_code(url: str) -> int:
-    async with aiohttp.ClientSession() as session:
-        async with session.head(url) as resp:
-            return resp.status
+    async with aiosession.head(url) as resp:
+        return resp.status
 
 
 async def make_carbon(code):
-    carbon = Carbon(code=code)
-    image = await carbon.save(str(randint(1000, 10000)))
+    url = "https://carbonara.vercel.app/api/cook"
+    async with aiosession.post(url, json={"code": code}) as resp:
+        image = BytesIO(await resp.read())
+    image.name = "carbon.png"
     return image
 
 
-async def transfer_sh(file):
+async def transfer_sh(file_or_message):
+    if isinstance(file_or_message, Message):
+        file_or_message = await file_or_message.download()
+    file = file_or_message
     async with aiofiles.open(file, "rb") as f:
         params = {file: await f.read()}
     async with aiohttp.ClientSession() as session:
-        async with session.post("https://transfer.sh/", data=params) as resp:
+        async with session.post(
+            "https://transfer.sh/", data=params
+        ) as resp:
             download_link = str(await resp.text()).strip()
     return download_link
 
@@ -157,7 +170,8 @@ def str_to_obj(string: str):
 async def calc_distance_from_ip(ip1: str, ip2: str) -> float:
     Radius_Earth = 6371.0088
     data1, data2 = await gather(
-        fetch(f"http://ipinfo.io/{ip1}"), fetch(f"http://ipinfo.io/{ip2}")
+        get(f"http://ipinfo.io/{ip1}"),
+        get(f"http://ipinfo.io/{ip2}"),
     )
     lat1, lon1 = data1["loc"].split(",")
     lat2, lon2 = data2["loc"].split(",")
@@ -165,7 +179,129 @@ async def calc_distance_from_ip(ip1: str, ip2: str) -> float:
     lat2, lon2 = radians(float(lat2)), radians(float(lon2))
     dlon = lon2 - lon1
     dlat = lat2 - lat1
-    a = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
+    a = (
+        sin(dlat / 2) ** 2
+        + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
+    )
     c = 2 * atan2(sqrt(a), sqrt(1 - a))
     distance = Radius_Earth * c
     return distance
+
+
+def get_urls_from_text(text: str) -> bool:
+    regex = r"""(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]
+                [.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(
+                \([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\
+                ()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))""".strip()
+    return [x[0] for x in findall(regex, text)]
+
+
+async def time_converter(message: Message, time_value: str) -> int:
+    unit = ["m", "h", "d"]  # m == minutes | h == hours | d == days
+    check_unit = ''.join(list(filter(time_value[-1].lower().endswith, unit)))
+    currunt_time = datetime.now()
+    time_digit = time_value[:-1]
+    if not time_digit.isdigit():
+        return await message.reply_text("Incorrect time specified")
+    if check_unit == "m":
+        temp_time = currunt_time + timedelta(
+            minutes=int(time_digit)
+        )
+    elif check_unit == "h":
+        temp_time = currunt_time + timedelta(
+            hours=int(time_digit)
+        )
+    elif check_unit == "d":
+        temp_time = currunt_time + timedelta(
+            days=int(time_digit)
+        )
+    else:
+        return await message.reply_text("Incorrect time specified.")
+    return int(datetime.timestamp(temp_time))
+
+
+async def extract_userid(message, text: str):
+    text = text.strip()
+    entities = message.entities
+    app = message._client
+    if len(entities) < 2:
+        return (await app.get_users(text)).id
+    entity = entities[1]
+    if entity.type == "mention":
+        return (await app.get_users(text)).id
+    if entity.type == "text_mention":
+        return entity.user.id
+    return None
+
+
+async def extract_user_and_reason(message):
+    args = message.text.strip().split()
+    text = message.text
+    user = None
+    reason = None
+    if message.reply_to_message:
+        reply = message.reply_to_message
+        # if reply to a message and no reason is given
+        if not reply.from_user:
+            return None, None
+        if len(args) < 2:
+            reason = None
+        else:
+            reason = text.split(None, 1)[1]
+        return reply.from_user.id, reason
+
+    # if not reply to a message and no reason is given
+    if len(args) == 2:
+        user = text.split(None, 1)[1]
+        return await extract_userid(message, user), None
+    # if reason is given
+    elif len(args) > 2:
+        user, reason = text.split(None, 2)[1:]
+        return await extract_userid(message, user), reason
+
+    return user, reason
+
+
+async def extract_user(message):
+    return (await extract_user_and_reason(message))[0]
+
+
+async def test_ARQ(message):
+    results = ""
+    funcs = {
+        "deezer": arq.deezer("attention", 1),
+        "image": arq.image("something"),
+        "luna": arq.luna("hello"),
+        "lyrics": arq.lyrics("attention"),
+        "nlp": arq.nlp("bitcoin"),
+        "nsfw_scan": arq.nsfw_scan(
+            url="https://www.pixsy.com/wp-content/uploads/2021/04/ben-sweet-2LowviVHZ-E-unsplash-1.jpeg"
+        ),
+        "pornhub": arq.pornhub("something"),
+        "proxy": arq.proxy(),
+        "pypi": arq.pypi("python-arq"),
+        "reddit": arq.reddit("badcode"),
+        "quotly": arq.quotly(message),
+        "saavn": arq.saavn("attention"),
+        "stats": arq.stats(),
+        "tmdb": arq.tmdb("flash"),
+        "torrent": arq.torrent("porn"),
+        "translate": arq.translate("hello"),
+        "urbandict": arq.urbandict("wtf"),
+        "wall": arq.wall("anime"),
+        "wiki": arq.wiki("cat"),
+        "youtube": arq.youtube("never gonna give you up"),
+    }
+
+    for key, value in funcs.items():
+        try:
+            t1 = time()
+            result = await value
+            t2 = time()
+            if result.ok:
+                results += f"**{key.capitalize()}:** `{t2-t1}`\n"
+            else:
+                results += f"**{key.capitalize()}:** `Failed`\n"
+        except Exception:
+            results += f"**{key.capitalize()}:** `Failed`\n"
+    return results
