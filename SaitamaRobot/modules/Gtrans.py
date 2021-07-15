60d9ae347104b10e501c593d311730c3a8f80443
Deadpool <79030362+SatishMarisetty@users.noreply.github.com>
2021-07-15 22:15:45 +0530

    Update Gtrans.py

--- SaitamaRobot/modules/Gtrans.py
+++ SaitamaRobot/modules/Gtrans.py
@@ -2,6 +2,7 @@ from SaitamaRobot import pbot as app
 from SaitamaRobot import arq
 from SaitamaRobot.utils.errors import capture_err
 from pyrogram import filters
+from telegram import ParseMode
 
 
 @app.on_message(filters.command("tr") & ~filters.edited)
@@ -25,8 +26,14 @@ async def tr(_, message):
         )
     result = await arq.translate(text, lang)
     output = result.result.translatedText
+    error = f"""**Error occurred!**
+Get supported language codes from [here](https://developers.google.com/admin-sdk/directory/v1/languages)"""
     msg = f"""**Successfully translated to {lang}:**
 `{output}`"""
     if not result.ok:
         return await message.reply_text(result.result)
     await message.reply_text(msg)
+        except AttributeError:
+            message.reply_text(
+                error, parse_mode=ParseMode.HTML, disable_web_page_preview=True,
+            )
