876c15093758644928b1c3ee34e142e9218e418a
Deadpool <79030362+SatishMarisetty@users.noreply.github.com>
2021-07-17 09:54:01 +0530

    Update gtranslator.py

--- SaitamaRobot/modules/gtranslator.py
+++ SaitamaRobot/modules/gtranslator.py
@@ -1,107 +1,47 @@
 from emoji import UNICODE_EMOJI
-from google_trans_new import LANGUAGES, google_translator
+ #from google_trans_new import LANGUAGES, google_translator
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, run_async
+from googletrans import Translator
 
 from SaitamaRobot import dispatcher
 from SaitamaRobot.modules.disable import DisableAbleCommandHandler
+from SaitamaRobot.events import register
 
 
-@run_async
-def totranslate(update: Update, context: CallbackContext):
-    message = update.effective_message
-    problem_lang_code = []
-    for key in LANGUAGES:
-        if "-" in key:
-            problem_lang_code.append(key)
-
-    try:
-        if message.reply_to_message:
-            args = update.effective_message.text.split(None, 1)
-            if message.reply_to_message.text:
-                text = message.reply_to_message.text
-            elif message.reply_to_message.caption:
-                text = message.reply_to_message.caption
-
-            try:
-                source_lang = args[1].split(None, 1)[0]
-            except (IndexError, AttributeError):
-                source_lang = "en"
-
-        else:
-            args = update.effective_message.text.split(None, 2)
-            text = args[2]
-            source_lang = args[1]
-
-        if source_lang.count("-") == 2:
-            for lang in problem_lang_code:
-                if lang in source_lang:
-                    if source_lang.startswith(lang):
-                        dest_lang = source_lang.rsplit("-", 1)[1]
-                        source_lang = source_lang.rsplit("-", 1)[0]
-                    else:
-                        dest_lang = source_lang.split("-", 1)[1]
-                        source_lang = source_lang.split("-", 1)[0]
-        elif source_lang.count("-") == 1:
-            for lang in problem_lang_code:
-                if lang in source_lang:
-                    dest_lang = source_lang
-                    source_lang = None
-                    break
-            if dest_lang is None:
-                dest_lang = source_lang.split("-")[1]
-                source_lang = source_lang.split("-")[0]
-        else:
-            dest_lang = source_lang
-            source_lang = None
-
-        exclude_list = UNICODE_EMOJI.keys()
-        for emoji in exclude_list:
-            if emoji in text:
-                text = text.replace(emoji, "")
-
-        trl = google_translator()
-        if source_lang is None:
-            detection = trl.detect(text)
-            trans_str = trl.translate(text, lang_tgt=dest_lang)
-            return message.reply_text(
-                f"Translated from `{detection[0]}` to `{dest_lang}`:\n`{trans_str}`",
-                parse_mode=ParseMode.MARKDOWN,
-            )
-        else:
-            trans_str = trl.translate(text, lang_tgt=dest_lang, lang_src=source_lang)
-            message.reply_text(
-                f"Translated from `{source_lang}` to `{dest_lang}`:\n`{trans_str}`",
-                parse_mode=ParseMode.MARKDOWN,
-            )
-
-    except IndexError:
-        update.effective_message.reply_text(
-            "Reply to messages or write messages from other languages ​​for translating into the intended language\n\n"
-            "Example: `/tr en-ml` to translate from English to Malayalam\n"
-            "Or use: `/tr ml` for automatic detection and translating it into Malayalam.\n"
-            "See [List of Language Codes](t.me/OnePunchSupport/12823) for a list of language codes.",
-            parse_mode="markdown",
-            disable_web_page_preview=True,
-        )
-    except ValueError:
-        update.effective_message.reply_text("The intended language is not found!")
+@@register(
+    pattern="tl",
+)
+async def _(event):
+    if len(event.text) > 3:
+        if not event.text[3] == " ":
+            return
+    input = event.text[4:6]
+    txt = event.text[7:]
+    xx = await event.reply("`Translating...`")
+    if event.reply_to_msg_id:
+        previous_message = await event.get_reply_message()
+        text = previous_message.message
+        lan = input or "en"
+    elif input:
+        text = txt
+        lan = input or "en"
     else:
-        return
+        return await eod(xx, f"`/tr LanguageCode` as reply to a message", time=5)
+    translator = Translator()
+    try:
+        tt = translator.translate(text, dest=lan)
+        output_str = f"**TRANSLATED** from {tt.src} to {lan}\n{tt.text}"
+        await eor(xx, output_str)
+    except Exception as exc:
+        await eod(xx, str(exc), time=10)
 
 
 __help__ = """
 • `/tr` or `/tl` (language code) as reply to a long message
 *Example:*
   `/tr en`*:* translates something to english
-  `/tr hi-en`*:* translates hindi to english
+  `/tl hi-en`*:* translates hindi to english
             [all available language codes](https://developers.google.com/admin-sdk/directory/v1/languages)
 """
-
-TRANSLATE_HANDLER = DisableAbleCommandHandler("tl", totranslate)
-
-dispatcher.add_handler(TRANSLATE_HANDLER)
-
 __mod_name__ = "G-TRANS"
-__command_list__ = ["tl"]
-__handlers__ = [TRANSLATE_HANDLER]
