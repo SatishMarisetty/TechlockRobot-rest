22371470cf5d57577730c31e4effb8d8c8d043a4
Deadpool <79030362+SatishMarisetty@users.noreply.github.com>
2021-07-17 19:33:19 +0530

    Update Gtrans.py

--- SaitamaRobot/modules/Gtrans.py
+++ SaitamaRobot/modules/Gtrans.py
@@ -42,6 +42,6 @@ async def wall(_, message):
 
     if not results.ok:
         return await message.reply_text(results.result)
-    results = results.result[0:2]
+    results = results.result[5]
     for i in results:
             await message.reply_text(f"{i.url_image}")
