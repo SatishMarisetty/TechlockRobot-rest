737dd97df8845f649026c254befc8b04c365ca8b
Deadpool <79030362+SatishMarisetty@users.noreply.github.com>
2021-07-18 20:36:28 +0530

    Update ARQspecial.py

--- SaitamaRobot/modules/ARQspecial.py
+++ SaitamaRobot/modules/ARQspecial.py
@@ -34,13 +34,34 @@ Get supported language codes from [here](https://developers.google.com/admin-sdk
 
 @app.on_message(filters.command("wall") & ~filters.edited)
 @capture_err
-async def wall(_, message):
-    query = message.text[len("/wall ") :]
+async def wall(update: Update, context: CallbackContext):
+    chat_id = update.effective_chat.id
+    msg = update.effective_message
+    args = context.args
+    msg_id = update.effective_message.message_id
+    bot = context.bot
+    query = " ".join(args)
     if not query:
-        return await message.reply_text("Enter a query to search!")
-    results = await arq.wall(query)
+        msg.reply_text("Please enter a query!")
+        return
+    else:
+        results = await arq.wall(query)
     if not results.ok:
-        return await message.reply_text("No wallpaper found! Refine your search.")
-    n = random.randint(1,29)
-    results = results.result[(n):(n)+1]
-            await message.reply_text(results.url_image)
+        return await msg.reply_text("No wallpaper found! Refine your search.")
+    n = randint(1,29)
+    reslts = results.result[(n):(n)+1].url_image
+                bot.send_photo(
+                    chat_id,
+                    photo=reslts,
+                    caption="Preview",
+                    reply_to_message_id=msg_id,
+                    timeout=60,
+                )
+                bot.send_document(
+                    chat_id,
+                    document=reslts,
+                    filename="wallpaper",
+                    caption=query,
+                    reply_to_message_id=msg_id,
+                    timeout=60,
+                )
