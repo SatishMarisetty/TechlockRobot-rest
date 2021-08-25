879915a2ca29d22c9404ee8378a9855f09ce1920
Deadpool <79030362+SatishMarisetty@users.noreply.github.com>
2021-08-25 07:13:16 +0530

    Update forcesubscribe.py

--- TechlockRobot/modules/forcesubscribe.py
+++ TechlockRobot/modules/forcesubscribe.py
@@ -177,23 +177,3 @@ def config(client, message):
         message.reply_text(
             "❗ **Group Creator Required**\n__You have to be the group creator to do that.__"
         )
-
-
-__help__ = """
-*Force Subscribe:*
-• I can mute members who are not subscribed your channel until they subscribe
-• When enabled I will mute unsubscribed members and show them a unmute button. When they pressed the button I will unmute them
-*Setup*
-*Only creator*
-• Add me in your group as admin
-• Add me in your channel as admin 
- 
-*Commmands*
- • /fsub {channel username} - To turn on and setup the channel.
-  💡Do this first...
- • /fsub - To get the current settings.
- • /fsub disable - To turn of ForceSubscribe..
-  💡If you disable fsub, you need to set again for working.. /fsub {channel username} 
- • /fsub clear - To unmute all members who muted by me.
-"""
-__mod_name__ = "FORCE SUB"
