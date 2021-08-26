5986acd4acb111450de34c05e5b4234547374443
Deadpool <79030362+SatishMarisetty@users.noreply.github.com>
2021-08-26 06:48:00 +0530

    Update misc.py

--- TechlockRobot/modules/helper_funcs/misc.py
+++ TechlockRobot/modules/helper_funcs/misc.py
@@ -71,7 +71,7 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
              EqInlineKeyboardButton("↪", callback_data="{}_next({})".format(prefix, modulo_page)))]
 
     else:
-        pairs += [[EqInlineKeyboardButton("🔙", callback_data="saitamaa_back")]]
+        pairs += [[EqInlineKeyboardButton("🔙", callback_data="saitama_back")]]
 
     return pairs
 
