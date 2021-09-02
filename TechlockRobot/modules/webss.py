c98bf6cdf224844c7c7cd976dae15c6cf373f3b2
Deadpool <79030362+SatishMarisetty@users.noreply.github.com>
2021-09-02 17:12:36 +0530

    Update webss.py

--- TechlockRobot/modules/webss.py
+++ TechlockRobot/modules/webss.py
@@ -1,26 +1,25 @@
 from pyrogram import filters
+from pyrogram.types import Message
 
 from pyrogram import Client as app
-from TechlockRobot.utils.errors import capture_err
 
 @app.on_message(filters.command("webss"))
-@capture_err
-async def take_ss(_, message):
+async def take_ss(_, message: Message):
     try:
         if len(message.command) != 2:
-            await message.reply_text("Give A Url To Fetch Screenshot.")
-            return
+            return await message.reply_text(
+                "Give A Url To Fetch Screenshot."
+            )
         url = message.text.split(None, 1)[1]
         m = await message.reply_text("**Taking Screenshot**")
         await m.edit("**Uploading**")
         try:
-            await app.send_photo(
-                message.chat.id,
+            await message.reply_photo(
                 photo=f"https://webshot.amanoteam.com/print?q={url}",
+                quote=False,
             )
         except TypeError:
-            await m.edit("No Such Website.")
-            return
+            return await m.edit("No Such Website.")
         await m.delete()
     except Exception as e:
         await message.reply_text(str(e))
