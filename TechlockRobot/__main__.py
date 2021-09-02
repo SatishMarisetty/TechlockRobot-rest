f21634195b4e1c0ebc7f582da09d2f7fee92e5dc
Deadpool <79030362+SatishMarisetty@users.noreply.github.com>
2021-09-02 11:30:58 +0530

    Update __main__.py

--- TechlockRobot/__main__.py
+++ TechlockRobot/__main__.py
@@ -1,7 +1,7 @@
 import requests
 from pyrogram import Client as Bot
 from TechlockRobot import API_HASH, API_ID, TOKEN
-
+from TechlockRobot.run import run
 
 bot = Bot(
     ":memory:",
@@ -12,3 +12,4 @@ bot = Bot(
 )
 
 bot.start()
+run()
