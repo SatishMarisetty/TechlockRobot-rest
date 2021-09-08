931c06c69c35563e26bba30022d22840b4a04c7e
Deadpool <79030362+SatishMarisetty@users.noreply.github.com>
2021-09-08 09:56:55 +0530

    Update ascii.py

--- TechlockRobot/modules/ascii.py
+++ TechlockRobot/modules/ascii.py
@@ -10,16 +10,9 @@ from img2html.converter import Img2HTMLConverter
 async def ascii_func(_, e):
     if not e.reply_to_message:
         return await e.reply_text("`Reply to image.`")
-        media = e.photo
-    if not media:
-        reply = e.reply_to_message
-        if not getattr(reply, 'empty', True):
-            media = reply.photo
-    if not media:
-        await e.reply_text('Reply it to a Photo to work')
-        return
     m = await e.reply_text("`Converting to html...`")
-    img = await (await media.download_media()
+    media = e.reply_to_message.photo
+    img = await (await media).download_media()
     converter = Img2HTMLConverter(char="■")
     html = converter.convert(img)
     with open("html.html", "w") as t:
